5f3ab28718f0a0b7861c14ba09f7964ebdb70ce5
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-06-28 20:25:12 +0700

    Pertemuan 2

--- property dan method/produk.php
+++ property dan method/produk.php
@@ -1,12 +1,41 @@
 <?php 
 
 
-class Produk{
-    // ini adalah class
+class Produk{   // ini adalah class
+    // Membuat property
+    public   $jenis ="Jenis", 
+            $nama = "Nama",
+            $penulis = "Penulis",
+            $penerbit = "Penerbit",
+            $harga = "Harga";
+
+
+    // Memuat Method
+    public function getLabel(){
+        return "$this->jenis, $this->nama, $this->penulis";
+    }
+
 }
 
 
-$produk1 = new Produk(); //Ini adalah instance
+$produk1 = new Produk(); //Ini adalah 
+$produk1->jenis = "Game";
+$produk1->nama = "Naruto";
+$produk1->penulis = "Rizky";
+$produk1->penerbit = "Riski Corp";
+$produk1->harga = 30000;
+var_dump($produk1);
+echo $produk1->getLabel();
+echo "<br>";
+
+$produk2 = new Produk();
+$produk2->jenis="Film";
+$produk2->nama="Naruto";
+$produk2->penulis="Riski";
+$produk2->penerbit="Riski Corp";
+$produk2->harga=40000;
+var_dump($produk2);
+echo $produk2->getLabel();
 
 
 ?>
